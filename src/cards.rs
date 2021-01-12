a9fee917496fe972be3c2340c57df3516ed1841c
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-01-12 10:09:54 -0500

    Update cards documentation

--- src/cards.rs
+++ src/cards.rs
@@ -1,6 +1,13 @@
 use rand::seq::SliceRandom;
 
 /// Returns a 52-card deck in order
+///
+/// # Examples
+///
+/// ```
+/// use twentyone::cards;
+/// let mut deck = cards::create_deck();
+/// ```
 pub fn create_deck() -> Vec<[char; 2]> {
     let mut deck = Vec::new();
     let suits = ['S', 'H', 'C', 'D'];
@@ -23,6 +30,13 @@ pub fn create_deck() -> Vec<[char; 2]> {
 /// # Arguments
 ///
 /// * `deck_count` - The amount of decks to be placed in the shoe
+///
+/// # Examples
+///
+/// ```
+/// use twentyone::cards;
+/// let mut shoe = cards::create_shoe(6);
+/// ```
 pub fn create_shoe(deck_count: u8) -> Vec<[char; 2]> {
     let mut shoe = Vec::new();
     for _ in 0..deck_count {
@@ -32,12 +46,36 @@ pub fn create_shoe(deck_count: u8) -> Vec<[char; 2]> {
 }
 
 /// Shuffles a deck or shoe into a random order
+///
+/// # Arguments
+///
+/// * `deck` - The deck or shoe to shuffle
+///
+/// # Examples
+///
+/// ```
+/// use twentyone::cards;
+/// let mut deck = cards::create_deck();
+/// cards::shuffle_deck(&mut deck);
+/// ```
 pub fn shuffle_deck(deck: &mut Vec<[char; 2]>) {
     let mut rng = rand::thread_rng();
     deck.shuffle(&mut rng);
 }
 
 /// Returns the first card from a deck or shoe, then removes it
+///
+/// # Arguments
+///
+/// * `deck` - The deck or shoe to draw from
+///
+/// # Examples
+///
+/// ```
+/// use twentyone::cards;
+/// let mut deck = cards::create_deck();
+/// let card = cards::draw_card(&mut deck);
+/// ```
 pub fn draw_card(deck: &mut Vec<[char; 2]>) -> Result<[char; 2], ()> {
     if deck.len() > 0 {
         let card = deck[0];
