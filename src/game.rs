8a0993afd017b71b1bb67bbfb0502687ed47c975
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-01-13 14:19:04 -0500

    Create Player and Dealer structs

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,156 @@
+/// Describes a blackjack dealer
+pub struct Dealer {
+    hand: Vec<[char; 2]>,
+    shoe: Vec<[char; 2]>,
+    players: Vec<Player>,
+}
+
+/// Describes a blackjack player
+pub struct Player {
+    money: i32,
+    hands: Vec<Vec<[char; 2]>>,
+}
+
+impl Dealer {
+    /// Returns a new Dealer
+    ///
+    /// # Arguments
+    ///
+    /// * `shoe` - The shoe (or decck) to draw from
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use twentyone::cards;
+    /// use twentyone::game::Dealer;
+    /// let shoe = cards::create_shoe();
+    /// let dealer = Dealer::new(shoe);
+    /// ```
+    pub fn dealer(shoe: Vec<[char; 2]>) -> Dealer {
+        Dealer {
+            hand: Vec::new(),
+            shoe: shoe,
+            players: Vec::new(),
+        }
+    }
+
+    /// Returns a reference to the dealer's hand
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use twentyone::cards;
+    /// use twentyone::game::Dealer;
+    /// let shoe = cards::create_shoe();
+    /// let dealer = Dealer::new(shoe);
+    /// let hand = dealer.hand();
+    /// ```
+    pub fn hand(&self) -> &Vec<[char; 2]> {
+        &self.hand
+    }
+
+    /// Returns a reference to the dealer's shoe
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use twentyone::cards;
+    /// use twentyone::game::Dealer;
+    /// let shoe = cards::create_shoe();
+    /// let dealer = Dealer::new(shoe);
+    /// let shoe = dealer.shoe();
+    /// ```
+    pub fn shoe(&self) -> &Vec<[char; 2]> {
+        &self.shoe
+    }
+
+    /// Returns a reference to the dealer's players
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use twentyone::cards;
+    /// use twentyone::game::Dealer;
+    /// let shoe = cards::create_shoe();
+    /// let dealer = Dealer::new(shoe);
+    /// let players = dealer.players();
+    /// ```
+    pub fn players(&self) -> &Vec<Player> {
+        &self.players
+    }
+
+    pub fn hand_mut(&mut self) -> &mut Vec<[char; 2]> {
+        &mut self.hand
+    }
+
+    pub fn shoe_mut(&mut self) -> &mut Vec<[char; 2]> {
+        &mut self.shoe
+    }
+
+    pub fn players_mut(&mut self) -> &mut Vec<Player> {
+        &mut self.players
+    }
+}
+
+impl Player {
+    /// Returns a new Player
+    ///
+    /// # Arguments
+    ///
+    /// * `money` - The amount of money to give the player
+    /// * `hands` - A Vector of hands (`Vec<[char; 2]>`)
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use twentyone::game::Player;
+    /// let player = Player::new(100);
+    /// ```
+    pub fn new(money: i32) -> Player {
+        Player {
+            money: money,
+            hands: Vec::new(),
+        }
+    }
+
+    /// Returns a reference to the player's money
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use twentyone::game::Player;
+    /// let player = Player::new(100);
+    /// assert_eq!(*player.money(), 100);
+    /// ```
+    pub fn money(&self) -> &i32 {
+        &self.money
+    }
+
+    /// Returns a reference to the player's hands
+    pub fn hands(&self) -> &Vec<Vec<[char; 2]>> {
+        &self.hands
+    }
+
+    /// Returns a mutable reference to the player's money
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use twentyone::game::Player;
+    /// let mut player = Player::new(100);
+    /// *player.money_mut() = 500;
+    /// assert_eq!(*player.money(), 500);
+    /// ```
+    pub fn money_mut(&mut self) -> &mut i32 {
+        &mut self.money
+    }
+
+    /// Returns a mutable reference to the player's hands
+    pub fn hands_mut(&mut self) -> &mut Vec<Vec<[char; 2]>> {
+        &mut self.hands
+    }
+}
+
 /// Returns the value of a hand
 ///
 /// # Arguments
